0d73de9113a93605592b7543d14ec4b6b9bf354f
Kaung Shein <nicholasling1412@gmail.com>
2021-02-14 22:00:54 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-text">
+        © 2020 - {new Date().getFullYear()} Kaung Nan Dar Shein. All rights
+        reserved, I suppose. Powered by insomnia and unemployed tears.
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default function Navbar() {
+  return (
+    <>
+      {/* <div className="overlay">
+        <div className="overlay-content">
+          <li>
+            <Link to="/code">I code</Link>
+          </li>
+          <li>
+            <Link to="/snap">I snap</Link>
+          </li>
+          <li>
+            <Link to="/about">I am</Link>
+          </li>
+        </div>
+      </div> */}
+      <div className="navbar-container">
+        <nav className="navbar">
+          <Link to="/" id="logo">
+            Kaung Nan Dar Shein
+          </Link>
+          <Link to="/" id="logo-alt">
+            KNDS
+          </Link>
+          <div id="mobile-nav">
+            <div className="menu-button">
+              <div className="menu-button-burger"></div>
+            </div>
+            <div className="overlay-background"></div>
+          </div>
+          <ul className="navbar-links">
+            <li>
+              <Link to="/code">I code</Link>
+            </li>
+            <li>
+              <Link to="/snap">I snap</Link>
+            </li>
+            <li>
+              <Link to="/about">I am</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
+}
