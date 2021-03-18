2981999bc0a2a547cc18a68d69a36249d86d327d
Kaung Shein <nicholasling1412@gmail.com>
2021-03-17 22:00:27 -0400

    Homepage hover setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,70 @@ import { Link } from "gatsby";
 
 import SEO from "../components/seo";
 
-const HomePage = () => (
-  <>
-    <SEO title="Home" />
-    <div className="homepage-container">
-      <div className="links-container">
-        <div className="logo-container">
-          <div className="logo-content">Kaung Nan Dar Shein</div>
-        </div>
-        <div className="links">
-          <div className="code">
-            <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
+const HomePage = () => {
+  var num = [],
+    i = 0,
+    len = 30;
+  while (++i <= len) num.push(i);
+
+  return (
+    <>
+      <SEO title="Home" />
+      <div className="homepage-container">
+        <div className="links-container">
+          <div className="logo-container">
+            <div className="logo-content">Kaung Nan Dar Shein</div>
           </div>
-          <div className="snap">
-            <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
-          </div>
-          <div className="design">
-            <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
-          </div>
-          <div className="about">
-            <Link to="/about">
-              <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
-            </Link>
+          <div className="links">
+            <div className="code">
+              <Link to="/about">
+                <div className="link-title">I code.</div>
+                <div className="link-hover">
+                  {num.map((x, index) => {
+                    return <span>CODE</span>;
+                  })}
+                </div>
+                <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
+              </Link>
+            </div>
+            <div className="snap">
+              <Link to="/about">
+                <div className="link-title">I snap.</div>
+                <div className="link-hover">
+                  {num.map((x, index) => {
+                    return <span>CODE</span>;
+                  })}
+                </div>
+                <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
+              </Link>
+            </div>
+            <div className="design">
+              <Link to="/about">
+                <div className="link-title">I design.</div>
+                <div className="link-hover">
+                  {num.map((x, index) => {
+                    return <span>CODE</span>;
+                  })}
+                </div>
+                <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
+              </Link>
+            </div>
+            <div className="about">
+              <Link to="/about">
+                <div className="link-title">I am.</div>
+                <div className="link-hover">
+                  {num.map((x, index) => {
+                    return <span>CODE</span>;
+                  })}
+                </div>
+                <img src="https://a.espncdn.com/photo/2020/0903/r740305_1296x729_16-9.jpg" />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default HomePage;
