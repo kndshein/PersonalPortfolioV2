c6b1b6fc8ddcf1d9d4a6bf7c6b4e1aa407d8e45e
Kaung Shein <nicholasling1412@gmail.com>
2021-03-25 17:48:20 -0400

    Added algorithm to automatically group photos in 2 in SnapGallery

--- src/templates/SnapGallery.js
+++ src/templates/SnapGallery.js
@@ -6,18 +6,35 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 const SnapGallery = ({ data }) => {
+  console.log(data.gallery.images);
+  let images = [];
+  while (data.gallery.images.length > 0) {
+    let current = [];
+    current.push(data.gallery.images.shift());
+    current.push(data.gallery.images.shift());
+    images.push(current);
+  }
+
   return (
     <>
       <Layout>
         <SEO title={data.gallery.galleryTitle} />
         <div className="snap-gallery">
-          {data.gallery.images.map((image, index) => {
+          {images.map((image, index) => {
             return (
-              <div className="image-container" key={index}>
-                <img
-                  src={image.gatsbyImageData.images.fallback.src}
-                  alt={image.description}
-                />
+              <div className="row-container" key={index}>
+                <div className="image-container">
+                  <img
+                    src={image[0].gatsbyImageData.images.fallback.src}
+                    alt={image[0].description}
+                  />
+                </div>
+                <div className="image-container">
+                  <img
+                    src={image[1].gatsbyImageData.images.fallback.src}
+                    alt={image[1].description}
+                  />
+                </div>
               </div>
             );
           })}
