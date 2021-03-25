067fbada2e2c0f733313e2fa83f3cc95cf0c70ef
Kaung Shein <nicholasling1412@gmail.com>
2021-03-24 21:08:03 -0400

    Images on Snap Gallery displayed + Set up for responsive images

--- src/pages/snap.js
+++ src/pages/snap.js
@@ -1,17 +1,59 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const SnapPage = () => {
+const SnapPage = ({ data }) => {
   return (
     <>
       <Layout>
         <SEO title="I Snap" />
-        <div>Poop</div>
+        <div className="snap-page">
+          <div className="cards-container">
+            {data.galleries.edges.map((gallery, index) => {
+              const image = gallery.node.images.filter(
+                (image) => image.title === gallery.node.coverImage
+              );
+              return (
+                <div className="card-container">
+                  <div className="flex-container">
+                    <div className="card-title" key={index}>
+                      {gallery.node.galleryTitle}
+                    </div>
+                    <GatsbyImage
+                      className="card-image"
+                      image={image[0].gatsbyImageData}
+                      alt={image[0].description}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </Layout>
     </>
   );
 };
 
 export default SnapPage;
+
+export const pageQuery = graphql`
+  query {
+    galleries: allContentfulSnapGallery {
+      edges {
+        node {
+          galleryTitle
+          coverImage
+          images {
+            title
+            description
+            gatsbyImageData(layout: FULL_WIDTH, formats: [JPG])
+          }
+        }
+      }
+    }
+  }
+`;
--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -2,24 +2,28 @@ import React from "react";
 import { graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
 const Gallery = ({ data }) => {
-  console.log(data.gallery.images);
   return (
-    <div>
-      <div>
-        {data.gallery.images.map((image, index) => {
-          return (
-            <div key={index}>
-              <GatsbyImage
-                image={image.gatsbyImageData}
-                alt={image.description}
-              />
-            </div>
-          );
-        })}
-      </div>
-      <div>poop</div>
-    </div>
+    <>
+      <Layout>
+        <SEO title={data.gallery.galleryTitle} />
+        <div>
+          {data.gallery.images.map((image, index) => {
+            return (
+              <div key={index}>
+                <GatsbyImage
+                  image={image.gatsbyImageData}
+                  alt={image.description}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </Layout>
+    </>
   );
 };
 
@@ -29,10 +33,11 @@ export const pageQuery = graphql`
   query($slug: String!) {
     gallery: contentfulSnapGallery(slug: { eq: $slug }) {
       slug
+      galleryTitle
       images {
         title
         description
-        gatsbyImageData(layout: FULL_WIDTH)
+        gatsbyImageData(layout: FULL_WIDTH, formats: [JPG])
       }
     }
   }
