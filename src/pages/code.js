dd9609a9853913bb9ec04142f493eb6fab041122
Kaung Shein <nicholasling1412@gmail.com>
2021-03-28 02:41:26 -0400

    CodeGallery card setup draft

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-const codePage = () => {
-  return <div>Poop</div>;
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const codePage = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout>
+      <SEO title="I Code" />
+      <div className="code-page">
+        <div className="cards-container">
+          {data.projects.edges.map((project, index) => {
+            return (
+              <div className="card-container" key={index}>
+                <div className="card-title">{project.node.title}</div>
+                <div className="card-image">
+                  <GatsbyImage
+                    className="image"
+                    image={project.node.image.gatsbyImageData}
+                    alt={project.node.image.description}
+                  />
+                </div>
+                <div className="card-overlay">{project.node.description}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default codePage;
+
+export const pageQuery = graphql`
+  query {
+    projects: allContentfulCodeGallery {
+      edges {
+        node {
+          title
+          image {
+            gatsbyImageData
+            description
+          }
+          preview
+          description
+          features: childContentfulCodeGalleryFeaturesJsonNode {
+            Features
+          }
+          links: childContentfulCodeGalleryLinkJsonNode {
+            GitHub
+            Livelink
+          }
+          technologies: childContentfulCodeGalleryTechnologiesJsonNode {
+            Technologies
+          }
+        }
+      }
+    }
+  }
+`;
