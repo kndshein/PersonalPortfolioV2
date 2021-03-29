5eeae4ed83f7881dafc7c71ba9c82ea241e73b46
Kaung Shein <nicholasling1412@gmail.com>
2021-03-29 01:35:45 -0400

    Start page 404 setup

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,35 @@
 import React from "react";
+import jeffsum from "jeffsum";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage = () => {
+  const [paragraphs] = React.useState([]);
+  for (let i = 0; i < 10; i++) {
+    paragraphs.push(jeffsum(6));
+  }
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>404: Poop.</h1>
+      <h4>Something went wrong.</h4>
+      <p>
+        Since you're here, might as well read about that one time I pooped my
+        pants.
+      </p>
+      <p>
+        Oh, and here's randomly generated Jeff Goldblum text placeholder from{" "}
+        <a href="https://jeffsum.com" target="_blank" rel="noreferrer">
+          Jeffsum
+        </a>
+      </p>
+      {paragraphs.map((paragraph, index) => {
+        return <p key={index}>{paragraph}</p>;
+      })}
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
