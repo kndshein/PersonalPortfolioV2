66ee37d25a4d607b966035117abb080b96a39eac
Kaung Shein <nicholasling1412@gmail.com>
2021-03-28 21:55:55 -0400

    added handleModal + close onClicks

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,59 +1,67 @@
 import React from "react";
 import { MdClose } from "react-icons/md";
 
-const Modal = ({ cardData }) => {
+const Modal = ({ cardData, showModal, handleModal }) => {
   console.log(cardData);
+
   return (
-    <div className="modal">
-      <div className="close-icon">
-        <MdClose color="white" size={40} />
-      </div>
-      <div className="card-container">
-        <div className="preview-container">
-          <video autoPlay loop muted>
-            <source src={cardData.preview.file.url} type="video/mp4" />
-          </video>
-        </div>
-        <div className="text-container">
-          <div className="title">{cardData.title}</div>
-          <div className="description">{cardData.description}</div>
-          <div className="features">
-            <div className="features-title">Features</div>
-            <ul className="features-list">
-              {cardData.features.Features.map((feature, index) => {
-                return <li key={index}>{feature}</li>;
-              })}
-            </ul>
+    <>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-background" onClick={() => handleModal()} />
+          <div className="close-icon" onClick={() => handleModal()}>
+            <MdClose color="white" size={40} />
           </div>
-          <div className="technologies">
-            <div className="technologies-title">Technologies</div>
-            <div className="technologies-list">
-              {cardData.technologies.Technologies.map((technology, index) => {
-                return <span key={index}>{technology}</span>;
-              })}
+          <div className="card-container">
+            <div className="preview-container">
+              <video autoPlay loop muted>
+                <source src={cardData.preview.file.url} type="video/mp4" />
+              </video>
+            </div>
+            <div className="text-container">
+              <div className="title">{cardData.title}</div>
+              <div className="description">{cardData.description}</div>
+              <div className="features">
+                <div className="features-title">Features</div>
+                <ul className="features-list">
+                  {cardData.features.Features.map((feature, index) => {
+                    return <li key={index}>{feature}</li>;
+                  })}
+                </ul>
+              </div>
+              <div className="technologies">
+                <div className="technologies-title">Technologies</div>
+                <div className="technologies-list">
+                  {cardData.technologies.Technologies.map(
+                    (technology, index) => {
+                      return <span key={index}>{technology}</span>;
+                    }
+                  )}
+                </div>
+              </div>
+              <div className="links">
+                <a
+                  className="live-link"
+                  href={cardData.links.Livelink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Visit Website
+                </a>
+                <a
+                  className="github-link"
+                  href={cardData.links.GitHub}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </div>
             </div>
           </div>
-          <div className="links">
-            <a
-              className="live-link"
-              href={cardData.links.Livelink}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Visit Website
-            </a>
-            <a
-              className="github-link"
-              href={cardData.links.GitHub}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/pages/code.js
+++ src/pages/code.js
@@ -9,16 +9,28 @@ import Modal from "../components/modal";
 const CodePage = ({ data }) => {
   const [showModal, setShowModal] = React.useState(false);
 
+  const handleModal = () => {
+    showModal ? setShowModal(false) : setShowModal(true);
+  };
+
   return (
     <>
-      <Modal cardData={data.projects.edges[0].node} />
+      <Modal
+        cardData={data.projects.edges[0].node}
+        showModal={showModal}
+        handleModal={handleModal}
+      />
       <Layout>
         <SEO title="I Code" />
         <div className="code-page">
           <div className="cards-container">
             {data.projects.edges.map((project, index) => {
               return (
-                <div className="card-container" key={index}>
+                <div
+                  className="card-container"
+                  key={index}
+                  onClick={() => handleModal()}
+                >
                   <div className="card-title">{project.node.title}</div>
                   <div className="card-image">
                     <GatsbyImage
