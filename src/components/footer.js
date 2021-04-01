146d844d266731a08c884fad8e5bfe500f2f2874
Kaung Shein <nicholasling1412@gmail.com>
2021-04-01 00:00:54 -0400

    added social links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { GrInstagram, GrLinkedinOption, GrMedium } from "react-icons/gr";
 
 export default function Footer() {
   return (
     <footer className="footer">
+      <div className="social-links">
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/kndshein/"
+            >
+              <GrLinkedinOption />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/durian.puberty/"
+            >
+              <GrInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://medium.com/@kndshein"
+            >
+              <GrMedium />
+            </a>
+          </li>
+        </ul>
+      </div>
       <div className="footer-text">
         All works © 2020 - {new Date().getFullYear()} Kaung Nan Dar Shein. All
         rights reserved, I suppose. Powered by insomnia and unemployed tears.
