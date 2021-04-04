52a2a03dc142f9cc2f3b72bc0e6939e727b6a521
Kaung Shein <nicholasling1412@gmail.com>
2021-04-04 16:01:40 -0400

    webpackconfig added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,14 @@
 const path = require("path");
 const slash = require("slash");
 
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === "production") {
+    actions.setWebpackConfig({
+      devtool: false,
+    });
+  }
+};
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return graphql(
