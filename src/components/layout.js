6fe5278053d00a1011077b1423c9b542cc97116e
Kaung Shein <nicholasling1412@gmail.com>
2021-10-12 02:05:23 -0400

    removed unused code - layout.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,8 +12,6 @@ import { motion } from "framer-motion";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
-// import "./layout.css"
-
 const Layout = ({ children }) => {
   return (
     <>
