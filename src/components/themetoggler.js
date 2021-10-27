5028e64feb2cd862d6f6fa8ae63de6e5ac5c2f3a
Kaung Shein <nicholasling1412@gmail.com>
2021-10-27 15:27:10 -0400

    ThemeToggler hover in dark mode fix

--- src/components/themetoggler.js
+++ src/components/themetoggler.js
@@ -5,17 +5,20 @@ import { IoMoonSharp } from "react-icons/io5";
 const ThemeToggle = () => {
   return (
     <ThemeToggler>
-      {({ theme, toggleTheme }) => (
-        <div
-          className={`theme-toggler ${theme === "dark" ? "dark" : ""}`}
-          onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
-          role="button"
-          tabIndex="0"
-          onKeyPress={() => toggleTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <IoMoonSharp />
-        </div>
-      )}
+      {({ theme, toggleTheme }) => {
+        console.log("theme", theme);
+        return (
+          <div
+            className="theme-toggler"
+            onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
+            role="button"
+            tabIndex="0"
+            onKeyPress={() => toggleTheme(theme === "dark" ? "light" : "dark")}
+          >
+            <IoMoonSharp />
+          </div>
+        );
+      }}
     </ThemeToggler>
   );
 };
